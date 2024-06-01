bc16128b8466a0b481bd21854e47ff38557972fe
deividas strole <deiwis@yahoo.com>
2024-06-01 09:55:20 -0700

    going forward

--- src/main/java/com/example/Hib/web/UserController.java
+++ src/main/java/com/example/Hib/web/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,9 +19,16 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/register")
-    public String createUser (ModelMap model) {
+    public String getCreateUser (ModelMap model) {
         model.put("user", new User());
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String postCreateUser (User user) {
+        System.out.println(user);
+        userService.createUser(user);
+        return "redirect:/register";  // after postmaping return redirect!
     }
 
     @GetMapping("/users")
