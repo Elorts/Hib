c31f33fd3d31e1330080b10856c33751d1a5f6d6
deividas strole <deiwis@yahoo.com>
2024-05-31 21:09:50 -0700

    v3

--- src/main/java/com/example/Hib/web/UserController.java
+++ src/main/java/com/example/Hib/web/UserController.java
@@ -1,4 +1,24 @@
 package com.example.Hib.web;
 
+import com.example.Hib.service.UserService;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public String getAllUsers(ModelMap model) {
+        List<User> users = userService.findAll();
+        model.put("users", users);
+        return "users";
+    }
 }
